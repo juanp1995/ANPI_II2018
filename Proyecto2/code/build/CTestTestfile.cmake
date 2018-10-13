0f8679afa1e0308df79fe6fa29affe552b0bd970
juanp1995 <juanp1995@gmail.com>
2018-10-13 10:40:37 -0600

    Finished

--- Proyecto2/code/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/juanp1995/ANPI_II2018/Proyecto2/code
-# Build directory: /home/juanp1995/ANPI_II2018/Proyecto2/code/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
-subdirs("benchmarks")
