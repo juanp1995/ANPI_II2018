f472c1138b6c787477bd8e155faf1897d2a5e178
juanp1995 <juanp1995@gmail.com>
2018-11-12 20:47:01 -0600

    Presentation example

--- Expo/Ejemplo/optimization.m
+++ Expo/Ejemplo/optimization.m
@@ -2,18 +2,37 @@
 
 
 # Area of trapezoid 
-#		                      	_
+# A = (b + B)/2 * h
+# 
+#	d = l*cos(a)
+# h = l*sin(a)
+# B = b + 2*d
+#						B	
+#	__--------------------__  _ 
 #		\                   /		|
 #		 \                 /		|
 #			\ l           l /			| h
 #		   \             /			|
 #		    \___________/a    	|
-#							b		
+#	--d----			b		
 #
 # With the constraint:
 # 		b + 2*l = 7 [meters]
-function z = Area(l,a)
-	z = (7 - (l.*2) + (l.*cos(a))) * (l.*sin(a));
+# 	--> b = 7 - 2*l
+
+
+
+
+#
+# Area of the gutter in terms
+# of the lenght of the sides 
+# and the angle on inclination
+#
+function z = Area(l,a,s)
+	b = s - l.*2;
+	z = (b + l.*cos(a))*l.*sin(a);
+	#z = (7 - (l.*2) + (l.*cos(a))) * (l.*sin(a));
+
 endfunction
 
 
@@ -22,7 +41,7 @@ endfunction
 # 
 #
 #
-function x = goldenSection(l, a, xl, xu)
+function x = goldenSection(l, a, s, xl, xu)
 	epsilon = 10^-7;
 	R = (sqrt(5)-1)/2;		# golden ratio
 	d = R*(xu - xl);
@@ -32,10 +51,10 @@ function x = goldenSection(l, a, xl, xu)
 	xopt = 0;
 	
 	if(l==" ")
-		f =@(x,y) Area(x,y);
+		f =@(x,y) Area(x,y, s);
 		y = a;
 	else
-		f = @(x,y) Area(y,x);
+		f = @(x,y) Area(y,x, s);
 		y = l;
 	endif;
 	
@@ -71,23 +90,26 @@ endfunction;
 # Due to the constraint, the lenght of l
 # can only be in the interval 0 < l < 3
 #
-function [l,a] = optimize()
+function [l,a] = optimize(s)
 
 	epsilon = 10^-7;
+	cons_l = s/2;
 	
-	current_a = 0.5;		# initial guess for the angle
+	##current_a = 0.5;		# initial guess for the angle
+	current_a = 10;
 	next_a = 0;
-	current_l = goldenSection(" ", current_a, 0, 3.5, 1.5);
+	current_l = goldenSection(" ", current_a, s, 0, cons_l, cons_l/2);
 	next_l = 0;
 	
-	MAX_ITERATIONS = 100;
+	MAX_ITERATIONS = 500;
 	
 	aa=[];
 	ll=[];
 	
 	for i = 1:MAX_ITERATIONS
-		next_a = goldenSection(current_l, " ", 0, pi/2, pi/4);
-		next_l = goldenSection(" ", next_a, 0, 3.5, 1.5);
+		next_a = goldenSection(current_l, " ", s, 0, pi/2, pi/4);
+		#next_a = goldenSection(current_l, " ", 0, 90, 45);
+		next_l = goldenSection(" ", next_a, s, 0, cons_l, cons_l/2);
 		
 		if(abs(next_a-current_a)<epsilon && abs(next_l-current_l)<epsilon)
 			break;
@@ -103,22 +125,22 @@ function [l,a] = optimize()
 	l = next_l;
 	a = next_a;
 	
-	printf("Lenght of b: %f \n", 6-2*l);
+	printf("Lenght of b: %f \n", s-2*l);
 	printf("Lenght of l: %f \n", l);
-	printf("Angle: %f \n", a);
-	printf("Area: %f \n", Area(l,a));
+	printf("Angle: %f \n", a*(180/pi));
+	printf("Area: %f \n", Area(l,a,s));
 
 endfunction
 
 
 function t = AreaPlot()
 
-	l = linspace(0,3,100);
-	a = linspace(0,pi/2,100);
+	l = linspace(-10,10,100);
+	a = linspace(-10,10,100);
 
 	[ll,aa] = meshgrid(l,a);
 
-	meshc(ll,aa, (ll.*sin(aa))*(6-(ll.*2)+(ll.*cos(aa))) );
+	mesh(ll,aa, (ll.*sin(aa))*(6-(ll.*2)+(ll.*cos(aa))) );
 
 endfunction
 
